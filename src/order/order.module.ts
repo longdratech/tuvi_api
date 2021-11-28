5df79cb6b2fae95a789d62cf70011777901dfeb5
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-11-28 18:51:15 +0700

    import prisma

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderGateway } from './order.gateway';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [OrderGateway, OrderService]
+  providers: [OrderGateway, OrderService, PrismaService]
 })
 export class OrderModule {}
